752a587fddf1fdfddf0ade32eabd084896e8eb30
Tyler <pannelltyler91@gmail.com>
2024-07-18 09:10:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BasicCard from './components/Card';
+import ResponsiveAppBar from './components/Nav';
+
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BasicCard/>
       </header>
     </div>
   );
